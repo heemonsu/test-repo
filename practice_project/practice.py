4dc4bce4e66d17a647347a8570f3784e19e9c8e8
Himanshu Raghuvanshi <hraghuvanshi@outrider.ai>
2024-02-27 06:12:12 +0530

    Add a function to check for odd number

--- practice_project/practice.py
+++ practice_project/practice.py
@@ -1,6 +1,9 @@
 def is_even(number):
     return number % 2 == 0
 
+def is_odd(number):
+    return number % 2 != 0
+
 if __name__ == '__main__':
     print(is_even(2)) # True
-    print(is_even(3)) # False
+    print(is_odd(3)) # True
